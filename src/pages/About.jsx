b84ee60affee747a7a7f34677e8ffdf02f52890f
sandeepspawar <singh.rajputsandeep4787@gmail.com>
2024-01-10 23:04:24 +0530

    change

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,6 @@ export const About = () => {
   }, []);
   return (
     <div>
-      <h1>helldlldl</h1>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
